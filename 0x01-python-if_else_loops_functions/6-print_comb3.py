cd8c26acf876db4fd0a2311da0301b84edcff946
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2023-08-09 14:33:46 -0700

    Numbers must be separated by ,, followed by a space

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 90, 10):
-    for j in range(i, i + 10):
-        if j < 89:
-            print("{:02d}, ".format(j), end='')
-        else:
-            print("{:02d}".format(j))
-    print(end='\b\b ')
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end="")
+        if i < 8 or j < 9:
+            print(", ", end="")
+    print()
