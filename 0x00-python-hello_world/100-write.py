c7e50f08a07f386d3b2ff118b5efbd8fac8f2e7e
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2023-08-14 04:03:56 -0700

    and that piece of art is useful - Dora Korpar

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+# followed by a new line
+
+# 1 write the input
+sys.stderr.write(message + '\n')
+sys.exit(1)
