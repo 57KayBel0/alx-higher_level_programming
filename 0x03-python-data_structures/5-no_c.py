bf47a2feaa6a897ec25afc0a9a494f3791b3bb1f
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2023-08-15 16:38:16 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    # Initialize an empty string to store the result
     result = ""
+    # Loop through each character in the input string
     for char in my_string:
-        if  char != 'c' and char != 'C':
-             result += char
+        # If the character is not c or C, append it to the result
+        if char != 'c' and char != 'C':
+            result += char
+    # Return the result
     return result
