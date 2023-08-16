3ee24a782b88955fffd7b59bd3755d898d500fd8
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2023-08-16 16:05:14 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    sum_result = add(a, b)
-    diff_result = sub(a, b)
-    prod_result = mul(a, b)
-    quot_result = div(a, b)
-
-    print("10 + 5 =", sum_result)
-    print("10 - 5 =", diff_result)
-    print("10 * 5 =", prod_result)
-    print("10 / 5 =", quot_result)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
