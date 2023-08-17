66c05543b0c393b4f97ea6c4f1e96033c4584c9c
57KayBel0 <makgaekabelomotshabi.kb@gmail.com>
2023-08-16 17:02:22 -0700

    program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-total = 0
-for arg in sys.argv[1:]:
-    try:
-        total += int(arg)   
-    except ValueError:
-        print("Invalid argument: {}".format(arg))
-        sys.exit(1)
+    total = 0
+    for arg in sys.argv[1:]:
+        try:
+            total += int(arg)
+        except ValueError:
+            print("Invalid argument: {}".format(arg))
+            sys.exit(1)
 
-print(total)
+    print(total)
